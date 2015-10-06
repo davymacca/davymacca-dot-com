4abe9b7a38f85c29a02cdfa6036c783e87691c6a
David McGeorge <david@bigredcircle.com>
2015-10-06 21:07:31 +0100

    feat(Portfolio): New styles and interaction for all portfolio items

--- src/modules/portfolio.js
+++ src/modules/portfolio.js
@@ -15,40 +15,98 @@ var Portfolio = function () {
 
     var currentItem = null;
     var $portfolio = $('.js-portfolio');
+    var $overlay = $('.js-overlay');
+    var largeScreen = false;
 
-    function setPortfolioItem (id) {
-        currentItem = id;
+    // large screen detection
+    // check to see if matchMedia exists - no point in a pollyfill as the fallback will work
+    if (window.matchMedia && matchMedia('screen and (min-width: 600px)').matches) {
+        largeScreen = true;
+        // large screen sliding requres some extra css
+        $overlay.addClass('slide-up-down');
     }
 
-    function setActiveNavState ($el) {
-        // sets a marker on the Nav
-        $portfolio.find('a').removeClass('is-active');
-        $el.addClass('is-active');
+    function setActiveDetailsState (id) {
+        // hide all items
+        $('.js-portfolio-item').removeClass('is-active');
+
+        // show correct item
+        $('#' + id).addClass('is-active');
+
+        Overlay.show();
     }
 
-    function setActiveDetailsState (newItemId) {
-        // shows the details for the selected portfolio item
 
-        function show (id, callback) {
-            var $el = $('#' + id);
-            $el.velocity({ opacity: 1 }, 200, callback);
-        }
 
-        function hide (id, callback) {
-            var $el = $('#' + id);
-            $el.velocity({ opacity: 0 }, 200, callback);
-        }
+    var Overlay = {
 
-        if (currentItem) {
-            hide(currentItem, function () {
-                show(newItemId);
+        setup: function () {
+
+            // get the true height (scollHeight) of the page element. Then subtract the height of the triangles
+            var trianglesHeight = $('.js-triangles').height();
+            var portfolioPageHeight = $('#work').find('.js-page')[0].scrollHeight;
+
+            // set height
+            $overlay.css('height', portfolioPageHeight - trianglesHeight);
+
+            // set top
+            $overlay.css('top', trianglesHeight);
+        },
+
+        resize: function () {
+
+        },
+
+        slideIn: function () {
+            $overlay.velocity({
+                translateX: [0, '-100%']
+            }, 300);
+        },
+
+        slideOut: function () {
+            $overlay.velocity({
+                translateX: ['-100%', 0]
+            }, 300);
+        },
+
+        slideUp: function () {
+            $overlay.css('display', 'block').velocity({
+                translateY: [0, '100%']
+            }, 300);
+        },
+
+        slideDown: function () {
+            $overlay.velocity({
+                translateY: ['100%', 0]
+            }, 300, function () {
+                $overlay.css('display', 'none');
             });
-        } else {
-            show(newItemId);
+        },
+
+        show: function () {
+            if (largeScreen) {
+                this.slideUp();
+            } else {
+                this.slideIn();
+            }
+        },
+
+        hide: function () {
+            if (largeScreen) {
+                this.slideDown();
+            } else {
+                this.slideOut();
+            }
         }
 
-    }
+    };
 
+
+
+    $overlay.on('click', '.js-close', function (event) {
+        event.preventDefault();
+        Overlay.hide();
+    });
 
     // Setup click events for the portfolio list
     $portfolio.on('click', 'a', function (event) {
@@ -59,13 +117,13 @@ var Portfolio = function () {
     });
 
     $portfolio.on('DMC.portfolioHasChanged', function (event, data) {
-        setActiveNavState(data.$el);
         setActiveDetailsState(data.id);
-        setPortfolioItem(data.id);
     });
 
 
 
+    // Setup the overlay
+    Overlay.setup();
 
 
 };
