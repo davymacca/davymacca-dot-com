d2d55e396132340fef9a7f91988c3a988f37e915
David McGeorge <david@bigredcircle.com>
2015-10-08 10:57:15 +0100

    feat(Gulp): added simple rror handling to watchify task so you dont have to restart gulp all the time

--- gulp/tasks/watchify.js
+++ gulp/tasks/watchify.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
@@ -17,6 +18,12 @@ module.exports = gulp.task('watchify', function () {
 
     function rebundle() {
         return bundler.bundle()
+            .on('error', function (err) {
+
+                gutil.log(gutil.colors.red("Browserify compile error:"), "\n\n", err.message, "\n");
+                gutil.beep();
+                this.emit('end'); // end this stream
+            })
             .pipe(source(global.config.filenames.build.scripts))
             .pipe(gulp.dest(global.config.paths.dest.build.scripts))
             .pipe(livereload());
